09a10a830a6831e586eaaae27c219d6bf8dc63c4
wktadmin <musicvae@gmail.com>
2018-11-12 13:14:39 +0800

    index

--- planet/models/index.py
+++ planet/models/index.py
@@ -4,24 +4,24 @@ from sqlalchemy import Column, Integer, String
 from planet.common.base_model import Base
 
 
-class IndexProductBrand(Base):
+class IndexBrand(Base):
     """
     首页显示的品牌
     """
-    __tablename__ = 'IndexProductBrand'
-    IPBid = Column(String(64), primary_key=True)
+    __tablename__ = 'IndexBrand'
+    IBid = Column(String(64), primary_key=True)
     PBid = Column(String(64), nullable=False, comment='商品品牌')
-    IPBsort = Column(Integer, comment='顺序标志')
+    IBsort = Column(Integer, comment='顺序标志')
 
 
-class IndexProductBrandItem(Base):
+class IndexBrandProduct(Base):
     """
     品牌推荐展示的商品
     """
-    __tablename__ = 'IndexProductBrandItem'
-    IPBIid = Column(String(64), primary_key=True)
+    __tablename__ = 'IndexBrandProduct'
+    IBPid = Column(String(64), primary_key=True)
     PRid = Column(String(64), primary_key=True, comment='商品')
-    IPBIsort = Column(Integer, comment='顺序')
+    IBPsort = Column(Integer, comment='顺序')
 
 
 class IndexBanner(Base):
