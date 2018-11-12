9a46552cfea94ff18b9216ea5f1fcadd17ba4bac
wktadmin <musicvae@gmail.com>
2018-11-12 18:36:57 +0800

    胡乱操作

--- planet/models/trade.py
+++ planet/models/trade.py
@@ -134,6 +134,6 @@ class OrderLogistics(Base):
 class LogisticsCompnay(Base):
     """快递公司"""
     __tablename__ = 'LogisticsCompnay'
-    _id = Column(String(64), primary_key=True)
-    LCname = Column(String(32), nullable=False, index=True, comment='公司名称')
-    LCcode = Column(String(32), nullable=False, index=True, comment='快递公司编码')
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    LCname = Column(String(64), nullable=False, index=True, comment='公司名称')
+    LCcode = Column(String(64), nullable=False, index=True, comment='快递公司编码')
