a2db67b47ea3f1a2f74bec774877724c08fd9779
wktadmin <musicvae@gmail.com>
2018-11-13 16:43:28 +0630

    todo order

--- planet/api/v1/ALogistic.py
+++ planet/api/v1/ALogistic.py
@@ -4,11 +4,18 @@ from planet.control.CLogistic import CLogistic
 
 
 class ALogistic(Resource):
+    """物流相关"""
     def __init__(self):
         self.clogistic = CLogistic()
 
     def get(self, logistic):
         apis = {
-            'list_company': self.clogistic.list_company
+            'list_company': self.clogistic.list_company,
+        }
+        return apis
+
+    def post(self, logistic):
+        apis = {
+            'subcribe_callback': self.clogistic.subcribe_callback,
         }
         return apis
