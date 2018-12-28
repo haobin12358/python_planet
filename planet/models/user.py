2ff944445aed8455d74dd053fa4ce794ed9198c2
wukt <wkt@wktadmin.com>
2018-12-28 19:46:12 +0800

    upgrade

--- planet/models/user.py
+++ planet/models/user.py
@@ -22,8 +22,10 @@ class User(Base):
     USopenid2 = Column(Text, comment='公众号2 openid')
     USsupper1 = Column(String(64), comment='一级代理商id')  #
     USsupper2 = Column(String(64), comment='二级代理商id')  #
+    USsupper3 = Column(String(64), comment='三级代理商id')  #
     USCommission1 = Column(Float, comment='佣金分成')       # 一级佣金分成比例
     USCommission2 = Column(Float, comment='佣金分成')       # 二级佣金分成比例
+    USCommission3 = Column(Float, comment='佣金分成')       # 三级佣金分成比例
     USintegral = Column(Integer, comment='积分')
     USlevel = Column(Integer, default=1, comment='等级 {1：普通游客，2：代理商, 3: 申请成代理商中}')
     USfrom = Column(Integer, default=1, comment='注册来源 {1: 微信h5, 2: app}')
