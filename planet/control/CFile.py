6bfcf3da0a16a36c0479f1843f288bfc31c95f36
Wiilz <rayleignze@gmail.com>
2019-01-04 21:11:23 +0800

    Fix News and TrialCommodity

--- planet/control/CFile.py
+++ planet/control/CFile.py
@@ -117,7 +117,9 @@ class CFile(object):
 
     def check_file_size(self):
         max_file_size = 20 * 1024 * 1024
-        if request.content_length > max_file_size:
+        upload_file_size = request.content_length
+        current_app.logger.info(">>>  Upload File Size is {0} MB <<<".format(round(upload_file_size/1048576, 2)))
+        if upload_file_size > max_file_size:
             raise ParamsError("上传文件过大，请上传小于20MB的文件")
 
     @staticmethod
