7414ffec89d73ab9125766c5ccdfb4121d863502
wukt <wkt@wktadmin.com>
2019-01-07 15:47:31 +0800

    meg

--- planet/control/CUser.py
+++ planet/control/CUser.py
@@ -771,7 +771,6 @@ class CUser(SUser, BASEAPPROVAL):
         check_result, check_reason = self.__check_qualifications(user)
         if check_result:
             # 资质认证ok，创建审批流
-
             avid = self.create_approval(self.APPROVAL_TYPE, request.user.id, request.user.id)
             auto_agree_task.apply_async(args=[avid], countdown=2 * 7, expires=10 * 60)
             # todo 遍历邀请历史，将未成为店主以及未成为其他店主粉丝的粉丝绑定为自己的粉丝在审批完成之后实现
