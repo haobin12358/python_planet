72aa06ff77d51105ffd8808a63718760a53a9ce3
shuaibinliu <13588046135@163.com>
2019-03-05 21:21:16 +0800

    测试环境部署回复

--- planet/config/http_config.py
+++ planet/config/http_config.py
@@ -2,15 +2,15 @@
 # HTTP_HOST = 'https://planet.daaiti.cn'
 # API_HOST = 'https://planet.daaiti.cn'
 # MEDIA_HOST = 'https://planet.daaiti.cn'
-HTTP_HOST = 'https://test.bigxingxing.com'
-API_HOST = 'https://test.bigxingxing.com'
-MEDIA_HOST = 'https://test.bigxingxing.com'
+HTTP_HOST = 'https://www.bigxingxing.com'
+API_HOST = 'https://www.bigxingxing.com'
+MEDIA_HOST = 'https://wwwbigxingxing.com'
 
 
 # 域名
 # PLANET_SERVICE = 'https://planet.daaiti.cn'
-PLANET_SERVICE = 'https://test.bigxingxing.com'
-PLANET_SUBSCRIBE = 'https://test.bigxingxing.com'
+PLANET_SERVICE = 'https://www.bigxingxing.com'
+PLANET_SUBSCRIBE = 'https://www.bigxingxing.com'
 # PLANET_SUBSCRIBE = 'https://planetsubscribe.daaiti.cn'
-PLANET = 'test.bigxingxing.com'
+PLANET = 'www.bigxingxing.com'
 # PLANET = 'https://planet.daaiti.cn'
