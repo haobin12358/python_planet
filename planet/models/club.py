493044dfe6a137fcf75ab8023eb463b8c8f869cb
haobin12358 <1276121237@qq.com>
2019-03-21 12:34:58 +0800

    官网涉及到的数据表

---
+++ planet/models/club.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Integer, String, Text, Float, Boolean, DateTime, DECIMAL
+from sqlalchemy.dialects.mysql import LONGTEXT
+from planet.common.base_model import Base, Column
+
+class CompanyMessage(Base):
+    """
+    公司公告
+    """
+    __tablename__ = "CompanyMessage"
+    CMid = Column(String(64), primary_key=True)
+    CMtitle = Column(String(255), nullable=False, comment="公告标题")
+    CMmessage = Column(LONGTEXT, nullable=False, comment="公告详情")
+
+class UserWords(Base):
+    """
+    官网留言
+    """
+    __tablename__ = "UserWords"
+    UWid = Column(String(64), primary_key=True)
+    UWmessage = Column(Text, nullable=False, comment="留言内容")
+    UWname = Column(String(64), comment="留言人姓名")
+    UWtelphone = Column(String(14), comment="留言人联系方式")
+    UWemail = Column(String(128), comment="留言人邮箱")
