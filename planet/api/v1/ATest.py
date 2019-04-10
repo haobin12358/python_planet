c48eae329c24124468dadb467001aa059843b725
Wiilz <rayleignze@gmail.com>
2019-04-10 15:43:02 +0800

    发货自动完成订单测试

--- planet/api/v1/ATest.py
+++ planet/api/v1/ATest.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from planet.common.base_resource import Resource
 from testcase.testdata.TDUsers import TDUsers
+from testcase.testdata.auto_complete_order import Test
 
 
 class ATest(Resource):
     def __init__(self):
         self.test = TDUsers()
+        self.test_temp = Test()
 
     def get(self, test):
         apis = {
             "test_user_data": self.test.test_user_data
+        }
+        return apis
+
+    def post(self, test):
+        apis = {
+            "auto_complete": self.test_temp.auto_complete_order,
         }
         return apis
