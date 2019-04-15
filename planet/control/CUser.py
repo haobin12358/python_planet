79f10972d319c8114921dd03f8329f78b77a5eda
Joerme <1210856886@qq.com>
2019-04-15 15:28:37 +0800

    转发 统一了下代码风格

--- planet/control/CUser.py
+++ planet/control/CUser.py
@@ -2166,42 +2166,40 @@ class CUser(SUser, BASEAPPROVAL):
             ss.add('createtime')
         return Success('获取结算记录成功', data=ss_list)
 
-    @get_session
     @token_required
     def transmit(self):
         """转发"""
-        usid = request.user.id
-        if usid:
-            user = self.get_user_by_id(usid)
-            current_app.logger.info('User {0} transmit '.format(user.USname))  # 在服务器打印日志
-        data = parameter_required(('Contentid', 'UTtype'))
-        contentid = data.get('Contentid')
-        uttype = data.get('Uttype')  # {1:资讯，2：商品，3：活动}
-        content_transmit = UserTransmit.create({
-                'UTid': str(uuid.uuid1()),
-                'Contentid': contentid,
-                'USid': usid,
-                'UTtype': uttype
-        })
-        db.session.add(content_transmit)
-        now_time = datetime.now()
-        count = db.query(UserTransmit).filter(
-                extract('month', UserTransmit.createtime) == now_time.month,
-                extract('year', UserTransmit.createtime) == now_time.year,
-                extract('day', UserTransmit.createtime) == now_time.day,
-                UserTransmit.USid == usid).count()
-        if count <= 5:
-            integral = '5'
-            # integral = ConfigSettings().get_item('integralbase', 'integral_transmit')
-            ui = UserIntegral.create({
-                    'UIid': str(uuid.uuid1()),
-                    'USid': usid,
-                    'UIintegral': integral,
-                    'UIaction': UserIntegralAction.transmit.value,
-                    'UItype': UserIntegralType.income.value
-                })
-            db.session.add(ui)
-            user.USintegral += int(ui.UIintegral)
+        user = self.get_user_by_id(request.user.id)
+        current_app.logger.info('User {0} transmit '.format(user.USname))  # 在服务器打印日志
+        data = parameter_required(('contentid', 'uttype'))
+        contentid = data.get('contentid')
+        uttype = data.get('uttype')  # {1:资讯，2：商品，3：活动}
+        with db.auto_commit():
+            content_transmit = UserTransmit.create({
+                    'UTid': str(uuid.uuid1()),
+                    'Contentid': contentid,
+                    'USid': user.USid,
+                    'UTtype': uttype
+            })
+            db.session.add(content_transmit)
+            now_time = datetime.now()
+            count = UserTransmit.query(UserTransmit).filter(
+                    extract('month', UserTransmit.createtime) == now_time.month,
+                    extract('year', UserTransmit.createtime) == now_time.year,
+                    extract('day', UserTransmit.createtime) == now_time.day,
+                    UserTransmit.USid == user.USid).count()
+            if count <= 5:
+                integral = '5'
+                # integral = ConfigSettings().get_item('integralbase', 'integral_transmit')
+                ui = UserIntegral.create({
+                        'UIid': str(uuid.uuid1()),
+                        'USid': user.USid,
+                        'UIintegral': integral,
+                        'UIaction': UserIntegralAction.transmit.value,
+                        'UItype': UserIntegralType.income.value
+                    })
+                db.session.add(ui)
+                user.USintegral += int(ui.UIintegral)
         return Success('转发成功')
 
 
