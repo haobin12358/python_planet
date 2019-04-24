41d0431060d82ce0b80b78d3c5f0de03b94d5d23
shuaibinliu <13588046135@163.com>
2019-04-25 04:05:38 +0800

    异常数据 捕获

--- planet/control/COrder.py
+++ planet/control/COrder.py
@@ -233,7 +233,10 @@ class COrder(CPay, CCoupon):
             )
         if createtime_start:
             query = query.filter(
-                OrderMain.createtime >= createtime_start,
+                or_(OrderMain.createtime >= createtime_start,
+                    OrderMain.updatetime >= createtime_start
+                    )
+
             )
         if createtime_end:
             query = query.filter(
