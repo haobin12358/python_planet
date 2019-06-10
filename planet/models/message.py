95e0861af169add7052d827e903937a8797526b2
shuaibinliu <13588046135@163.com>
2019-06-10 10:05:13 +0800

    增加实时交互

--- planet/models/message.py
+++ planet/models/message.py
@@ -6,6 +6,7 @@ from planet.common.base_model import Base, Column
 
 class PlatformMessage(Base):
     """站内信"""
+    __tablename__ = 'PlatformMessage'
     PMid = Column(String(64), primary_key=True)
     PMtext = Column(LONGTEXT, comment='站内信内容')
     PMtitle = Column(Text, comment='站内信标题')
@@ -16,6 +17,7 @@ class PlatformMessage(Base):
 
 class UserPlatfromMessage(Base):
     """个人站内信"""
+    __tablename__ = 'UserPlatfromMessage'
     UPMid = Column(String(64), primary_key=True)
     PMid = Column(String(64), comment='站内信id')
     USid = Column(String(64), comment='用户id')
@@ -24,6 +26,33 @@ class UserPlatfromMessage(Base):
 
 class UserPlatfromMessageLog(Base):
     """个人站内信阅读记录"""
+    __tablename__ = 'UserPlatfromMessageLog'
     UPMLid = Column(String(64), primary_key=True)
     UPMid = Column(String(64), comment='站内信id')
     USid = Column(String(64), comment='用户id')
+
+
+class UserMessage(Base):
+    """用户通讯记录"""
+    __tablename__ = 'UserMessage'
+    UMSGid = Column(String(64), primary_key=True)
+    # USsend = Column(String(64), comment='发送人')
+    # USreceive = Column(String(64), comment='接受人')
+    USid = Column(String(64), comment='发送人')
+    ROid = Column(String(64), comment='房间号')
+    UMSGtext = Column(LONGTEXT, comment='内容')
+
+
+
+class UserRoom(Base):
+    """用户对话房间"""
+    __tablename__ = 'UserRoom'
+    URid = Column(String(64), primary_key=True)
+    USid = Column(String(64), comment='参加用户')
+    ROid = Column(String(64), comment='房间号')
+
+
+class Room(Base):
+    """房间"""
+    __tablename__ = 'Room'
+    ROid = Column(String(64), primary_key=True)
