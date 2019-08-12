b30613d6c8c0469133a1722b1ba970ecfe5352c6
shuaibinliu <13588046135@163.com>
2019-08-12 15:02:10 +0800

    富文本创建+获取

--- planet/control/CIndex.py
+++ planet/control/CIndex.py
@@ -5,17 +5,19 @@ from flask import request, current_app
 from planet.common.error_response import SystemError, ParamsError
 from planet.common.params_validates import parameter_required
 from planet.common.success_response import Success
-from planet.common.token_handler import token_required, admin_required, is_admin
+from planet.common.token_handler import token_required, admin_required, is_admin, get_current_admin
 from planet.config.enums import ProductStatus, ProductBrandStatus, AdminActionS, MiniProgramBannerPosition
 from planet.control.BaseControl import BASEADMIN
 from planet.extensions.register_ext import cache, db
 from planet.extensions.validates.index import IndexListBannerForm, IndexSetBannerForm, IndexUpdateBannerForm
 from planet.models import Items, ProductBrand, BrandWithItems, Products, ProductItems, IndexBanner, \
-    HypermarketIndexBanner, Entry, Admin, MiniProgramBanner
+    HypermarketIndexBanner, Entry, Admin, MiniProgramBanner, LinkContent
 from planet.service.SIndex import SIndex
 
 
 class CIndex:
+    LCBASE = '/pages/travel/scenicDetail?sspid={}'
+
     def __init__(self):
         self.sindex = SIndex()
 
@@ -290,3 +292,47 @@ class CIndex:
                                                          MiniProgramBanner.createtime.desc()).all()
         [x.hide('ADid') for x in mpbs]
         return Success(data=mpbs)
+
+    @admin_required
+    def list_linkcontent(self):
+        lc_list = LinkContent.query.filter(LinkContent.isdelete == False).order_by(
+            LinkContent.createtime.desc()).all_with_page()
+        for lc in lc_list:
+            self._fill_lc(lc)
+        return Success(data=lc_list)
+
+    def _fill_lc(self, lc):
+        lc.fill('lclink', self.LCBASE.format(lc.LCid))
+
+    @admin_required
+    def set_linkcontent(self):
+        body = request.json
+        admin = get_current_admin()
+        current_app.logger.info('当前管理员是 {}'.format(admin.ADname))
+        lcid = body.get('lcid')
+        lccontent = body.get('lccontent')
+        with db.auto_commit():
+            if lcid:
+                lc = LinkContent.query.filter(LinkContent.LCid == lcid, LinkContent.isdelete == False).first()
+                if body.get('delete'):
+                    current_app.logger.info('开始删除富文本内容 lcid = {}'.format(lcid))
+                    if not lc:
+                        raise ParamsError('该内容已删除')
+                    lc.isdelete = True
+                    return Success('删除成功', data=lcid)
+                if lc:
+                    current_app.logger.info('开始更新富文本内容 lcid = {}'.format(lcid))
+                    if lccontent:
+                        lc.LCcontent = lccontent
+                    db.session.add(lc)
+                    return Success('更新成功', data=lcid)
+
+            if not lccontent:
+                raise ParamsError('富文本内容不能为空')
+            lc = LinkContent.create({
+                'LCid': str(uuid.uuid1()),
+                'LCcontent': lccontent
+            })
+            current_app.logger.info('开始创建富文本内容 lcid = {}'.format(lc.LCid))
+            db.session.add(lc)
+            return Success('添加成功', data=lc.LCid)
